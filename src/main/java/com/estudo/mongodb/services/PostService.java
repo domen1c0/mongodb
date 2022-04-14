36396e88015a1e7784946f747265e0552cc98d11
Paolo Domenico <paolodomenico777@gmail.com>
2022-04-14 13:11:53 -0300

    Post ID Search

--- src/main/java/com/estudo/mongodb/services/PostService.java
+++ src/main/java/com/estudo/mongodb/services/PostService.java
@@ -1,12 +1,14 @@
 package com.estudo.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.estudo.mongodb.domain.Post;
 import com.estudo.mongodb.repositories.PostRepository;
+import com.estudo.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
@@ -17,4 +19,9 @@ public class PostService {
 	public List<Post> findAll(){
 		return postRepository.findAll();
 	}	
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Post not found!"));
+	}
 }
